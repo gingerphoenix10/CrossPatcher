fc41d285eeda6224a460cac8ab8b76c334a440fb
Phoenix <phoenix@gingerphoenix10.com>
2026-04-02 01:25:46 +0100

    Rewritten to be cleaner, use a better method of patching, and support BepInEx / Thunderstore

--- CrossPatcher.cs
+++ CrossPatcher.cs
@@ -1,100 +1,39 @@
 using HarmonyLib;
-using Photon.Pun;
-using Photon.Realtime;
-using Portningsbolaget.Platforms;
 using Portningsbolaget.Utilities;
-using Unity.Mathematics;
-using UnityEngine;
-using Zorro.Settings;
-
-// ExampleCWPlugin is mostly an example of how to use the modding API, not an actual serious mod.
-// It adds a setting to the Mods settings page, which is a slider from 0 to 100.
-// It then edits the Flashlight.Update method to prevent the battery of the flashlight
-// from falling below that setting value.
+#if BEPIN
+using BepInEx;
+#endif
 
 namespace CrossPatcher;
 
-// The first argument is the GUID for this mod. This must be globally unique across all mods.
-// Consider prefixing your name/etc. to the GUID. (or generate an actual GUID)
-
-// vanillaCompatible: False means that this mod affects anything related to the multiplayer aspect of the game - for
-// example, adjusting sprint regen, battery life, changing monster behavior, or anything similar or more major. Most
-// mods leave this as false. 
-// True means that this mod only affects this client. For example, a mod that changes the folder your clips are saved at.
-// A good rule of thumb is: If someone else can tell you're using this mod, you must set vanillaCompatible to false.
-// If you set this to true, and it should be false, your mod may be removed/banned from the workshop.
-[ContentWarningPlugin("CrossPatcher", "0.1", vanillaCompatible: true )]
-public class CrossPatcher
-{
-    static CrossPatcher()
+[ContentWarningPlugin("CrossPatcher", "1.0.0", vanillaCompatible: true )]
+#if BEPIN
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class CrossPatcher : BaseUnityPlugin {
+    private static readonly Harmony Patcher = new(MyPluginInfo.PLUGIN_GUID);
+    private void Awake()
     {
-        // Static constructors of types marked with ContentWarningPluginAttribute are automatically invoked on load.
-        // Register callbacks, construct stuff, etc. here.
-        Debug.Log("Hello from CrossPatcher! This is called on plugin load");
-        // Adding the [ContentWarningSetting] attribute to a setting class is basically the same as:
-        // GameHandler.Instance.SettingsHandler.AddSetting(new ExampleSetting());
+        Patcher.PatchAll();
     }
 }
+#else
+public class CrossPatcher {}
+#endif
 
-[HarmonyPatch(typeof(MainMenuHandler))]
-public class MainMenuHandlerPatches
+[HarmonyPatch(typeof(Utilities))]
+public class UtilitiesPatches
 {
-    [HarmonyPatch(nameof(MainMenuHandler.OnLobbyHosted))]
-    [HarmonyPrefix]
-    private static bool OnLobbyHostedPrefix(MainMenuHandler __instance, ulong obj, bool privateMatch)
+    [HarmonyPatch(nameof(Utilities.CreateRandomName), new Type[]
     {
-        string localizedString = LocalizationKeys.GetLocalizedString(LocalizationKeys.Keys.Error_CreateRoom);
-        if (obj == 0L)
-        {
-            Debug.LogError("Something went wrong hosting the Lobby, aborting Photon Create Room");
-            Modal.ShowError(localizedString, "");
-            RetrievableSingleton<ConnectionStateHandler>.Instance.Disconnect();
-            return false;
-        }
-        Debug.Log($"Lobby Hosted: {obj} Hosting PhotonRoom Now");
-        LanguageMatchmakingSetting.MatchmakingLanguage value = (LanguageMatchmakingSetting.MatchmakingLanguage)GameHandler.Instance.SettingsHandler.GetSetting<PreferredLanguageMatchmakingSetting>().Value;
-        PlatformUtility.PlatformFamily platformFamily = (MainMenuHandler.CrossPlatform ? ((PlatformUtility.PlatformFamily)(-1)) : PlatformUtility.CurrentPlatform);
-        Debug.Log(string.Format("Targeted Platform: {0}", (platformFamily < PlatformUtility.PlatformFamily.None) ? "All" : ((object)platformFamily)));
-        RoomOptions roomOptions = new RoomOptions();
-        roomOptions.IsVisible = false;
-        roomOptions.MaxPlayers = 4;
-        roomOptions.Plugins = new string[1] { "ContentWarningPlugin" };
-        //roomOptions.Plugins = new string[0] { };
-        roomOptions.CustomRoomPropertiesForLobby = new string[4] { "P", "L", "PL", "M" };
-        roomOptions.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable
-        {
-            {
-                "C",
-                obj.ToString()
-            },
-            {
-                "P",
-                privateMatch ? 1 : 0
-            },
-            { "L", value },
-            {
-                "PL",
-                (int)platformFamily
-            },
-            {
-                "M",
-                "none"//GameHandler.GetPluginHash()
-            }
-        };
-        __instance.m_Hosting = false;
-        if (!PhotonNetwork.CreateRoom(Utilities.CreateRandomName(PhotonNetwork.CloudRegion, usingMods: false), roomOptions, TypedLobby.Default))
-        {
-            Debug.LogError("Error While Hosting Photon Room...");
-            Modal.ShowError(localizedString, "");
-            RetrievableSingleton<ConnectionStateHandler>.Instance.Disconnect();
-            return false;
-        }
-        Debug.Log("Created Photon Room... ");
-        if (!SaveSystem.HaveCurrentSave && SaveSystem.USING_SAVE)
-        {
-            SaveSystem.MakeNewSave();
-        }
-
-        return false;
+        typeof(string),
+        typeof(bool),
+        typeof(int),
+        typeof(bool)
+    })]
+    [HarmonyPrefix]
+    private static bool CreateRandomName(ref string region, ref bool usingMods, ref int length, ref bool upperCase)
+    {
+        usingMods = false;
+        return true;
     }
 }
